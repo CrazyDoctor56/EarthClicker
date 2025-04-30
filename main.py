189791b12894f58e413a8346124dd2aabf7ea929
kilex <kilex@gmail.com>
2025-04-30 19:11:54 +0300

    Version 0.2! New Update Please check ChangeLog.

--- main.py
+++ main.py
@@ -1,92 +1,103 @@
 import pygame
-import sys
-
-#SETTINGS
-HEIGHT = 1000
-WIDTH = 1100
-FPS = 60
-
-#COLOR
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-#GLOBAL
-energy = 0
-epc = 1
-upgrade_purchased = False
-new_planet_purchased = False
-rotation_angle = 0
-scale_factor = 1
-scaling = False
+from screeninfo import get_monitors
 
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
-#SCREEN
-sc = pygame.display.set_mode((WIDTH, HEIGHT))
+monitor = get_monitors()[0]
+WIDTH, HEIGHT = monitor.width, monitor.height
+FPS = 60
+
+WHITE = (255, 255, 255)
+
+energy = 999999
+base_epc = 1
+epc_bonus = 0
+epc_price = 200
+
+sc = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("Earth Clicker")
 pygame.display.set_icon(pygame.image.load("icon.png"))
 clock = pygame.time.Clock()
 
-#FONTS
 medium_font = pygame.font.SysFont("comicsans", 30)
-large_font = pygame.font.SysFont("comicsans", 50)
+small_font = pygame.font.SysFont("comicsans", 22)
 
-#IMAGES
 background = pygame.image.load("background.png")
-earth_lvl_1 = pygame.image.load("earth_lvl_1.png")
-earth_lvl_2 = pygame.image.load("earth_lvl_2.png")
-
-#SOUNDS
-pygame.mixer.music.load("background_sound.mp3")
-pygame.mixer.music.set_volume(0.1)
-pygame.mixer.music.play(-1)
 click_sound = pygame.mixer.Sound("click.mp3")
 purchase_sound = pygame.mixer.Sound("purchase.mp3")
 
-#CLASSES
-class Earth:
-    def __init__(self, image, x, y):
-        self.image = image
-        self.x = x
-        self.y = y
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.music.play(-1, 0.0)
 
-    def draw(self):
-        rotated_image = pygame.transform.rotate(self.image, rotation_angle)
-        rotated_rect = rotated_image.get_rect(center=self.rect.center)
+class Planet:
+    def __init__(self, image_path, price, epc):
+        self.image = pygame.image.load(image_path)
+        self.price = price
+        self.epc = epc
+        self.purchased = False
 
-        scaled_image = pygame.transform.scale(rotated_image, (int(rotated_image.get_width() * scale_factor), 
-                                                              int(rotated_image.get_height() * scale_factor)))
-        scaled_rect = scaled_image.get_rect(center=self.rect.center)
+planets = [
+    Planet("earth_lvl_1.png", 0, 1),
+    Planet("earth_lvl_2.png", 500, 5),
+    Planet("earth_lvl_3.png", 1500, 10),
+    Planet("earth_lvl_4.png", 3000, 20),
+]
 
-        sc.blit(scaled_image, scaled_rect.topleft)
+current_planet_index = 0
+current_planet = planets[current_planet_index]
 
-    def update(self):
-        global rotation_angle
-        global scale_factor, scaling
+rotation_angle = 0
+rotation_speed = 0.2
+scale_factor = 1
+scaling = False
+flash_texts = []
 
-        rotation_angle += 0.2
+earth_rect = current_planet.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 
-        if scaling:
-            scale_factor = 1.2
+upgrade_button_rect = pygame.Rect(WIDTH - 350, HEIGHT - 120, 300, 70)
+planet_button_rect = pygame.Rect(50, HEIGHT - 120, 300, 70)
+exit_button_rect = pygame.Rect(WIDTH - 150, 30, 100, 50)
+
+def draw_planet():
+    rotated_image = pygame.transform.rotate(current_planet.image, rotation_angle)
+    rotated_rect = rotated_image.get_rect(center=earth_rect.center)
+    scaled_image = pygame.transform.scale(rotated_image, (int(rotated_image.get_width() * scale_factor), int(rotated_image.get_height() * scale_factor)))
+    scaled_rect = scaled_image.get_rect(center=earth_rect.center)
+    sc.blit(scaled_image, scaled_rect.topleft)
+    return scaled_rect
+
+def draw_buttons():
+    pygame.draw.rect(sc, (50, 150, 220), planet_button_rect)
+    if current_planet_index < len(planets) - 1:
+        planet_price = planets[current_planet_index + 1].price
+        planet_label = "+New Planet" if not planets[current_planet_index + 1].purchased else "Next Selected"
+        sc.blit(medium_font.render(f"{planet_label}", 1, WHITE), (planet_button_rect.x + 20, planet_button_rect.y + 20))
+        sc.blit(small_font.render(f"{planet_price} Energy", 1, WHITE), (planet_button_rect.x + 20, planet_button_rect.y - 30))
+    else:
+        sc.blit(medium_font.render("No More Planets", 1, WHITE), (planet_button_rect.x + 20, planet_button_rect.y + 20))
+
+    pygame.draw.rect(sc, (50, 200, 50), upgrade_button_rect)
+    sc.blit(medium_font.render(f"+1 EPC", 1, WHITE), (upgrade_button_rect.x + 20, upgrade_button_rect.y + 20))
+    sc.blit(small_font.render(f"{epc_price} Energy", 1, WHITE), (upgrade_button_rect.x + 20, upgrade_button_rect.y - 30))
+
+    pygame.draw.rect(sc, (200, 50, 50), exit_button_rect)
+    sc.blit(medium_font.render("Exit", 1, WHITE), (exit_button_rect.x + 10, exit_button_rect.y + 10))
+
+def get_total_epc():
+    return base_epc + epc_bonus
+
+def draw_flash_texts():
+    for text, pos, alpha in flash_texts[:]:
+        surface = small_font.render(text, True, (255, 255, 0))
+        surface.set_alpha(alpha)
+        sc.blit(surface, pos)
+        new_alpha = alpha - 5
+        if new_alpha <= 0:
+            flash_texts.remove((text, pos, alpha))
         else:
-            scale_factor = 1
-
-earth_width = earth_lvl_1.get_width()
-earth_height = earth_lvl_1.get_height()
-
-earth = Earth(earth_lvl_1, (WIDTH - earth_width) // 2, (HEIGHT - earth_height) // 3)
-
-button_width = 300
-button_height = 80
-button_spacing = 20
-
-upgrade_button_rect = pygame.Rect((WIDTH - button_width) // 2, 850, button_width, button_height)
-new_planet_button_rect = pygame.Rect((WIDTH - button_width) // 2, 950, button_width, button_height)
-
-exit_button_rect = pygame.Rect(WIDTH - 170, 30, 140, 50)
+            flash_texts[flash_texts.index((text, pos, alpha))] = (text, (pos[0], pos[1]-1), new_alpha)
 
 game = True
 while game:
@@ -97,57 +108,49 @@ while game:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 pos = pygame.mouse.get_pos()
-                if earth.rect.collidepoint(pos):
+                planet_rect = draw_planet()
+                if planet_rect.collidepoint(pos):
                     click_sound.play()
-                    energy += epc
-
+                    earned = get_total_epc()
+                    energy += earned
+                    flash_texts.append((f"+{earned}", pos, 255))
                     scaling = True
 
-                if upgrade_button_rect.collidepoint(pos) and not upgrade_purchased and energy >= 200:
-                    energy -= 200
-                    epc = 2
-                    upgrade_purchased = True
+                if upgrade_button_rect.collidepoint(pos) and energy >= epc_price:
+                    energy -= epc_price
+                    epc_bonus += 1
+                    epc_price += 100
                     purchase_sound.play()
 
-                if new_planet_button_rect.collidepoint(pos) and not new_planet_purchased and energy >= 500:
-                    energy -= 500
-                    epc = 5
-                    new_planet_purchased = True
-                    earth.image = earth_lvl_2
-                    purchase_sound.play()
+                if planet_button_rect.collidepoint(pos) and current_planet_index < len(planets) - 1:
+                    next_planet = planets[current_planet_index + 1]
+                    if not next_planet.purchased and energy >= next_planet.price:
+                        energy -= next_planet.price
+                        next_planet.purchased = True
+                        current_planet_index += 1
+                        current_planet = next_planet
+                        earth_rect = current_planet.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+                        
+                        # Оновлюємо EPC після покупки нової планети
+                        epc_bonus += current_planet.epc
+                        purchase_sound.play()
 
                 if exit_button_rect.collidepoint(pos):
                     game = False
 
         if event.type == pygame.MOUSEBUTTONUP:
-            scaling = False
+            if event.button == 1:
+                scaling = False
 
     sc.blit(background, (0, 0))
-    earth.update()
-    earth.draw()
+    rotation_angle = (rotation_angle + rotation_speed) % 360
+    scale_factor = 1.05 if scaling else 1
+    draw_planet()
 
-    pygame.draw.rect(sc, (255, 50, 50), exit_button_rect)
-    exit_text = medium_font.render("Exit", 1, WHITE)
-    sc.blit(exit_text, (exit_button_rect.x + 35, exit_button_rect.y + 10))
+    sc.blit(medium_font.render(f"Energy: {energy}", 1, WHITE), (50, 50))
 
-    energy_text = medium_font.render(f"Energy: {energy}", 1, WHITE)
-    sc.blit(energy_text, (WIDTH // 2 - 70, 100))
-
-    color = (180, 180, 180) if upgrade_purchased else (50, 200, 50)
-    pygame.draw.rect(sc, color, upgrade_button_rect)
-    if upgrade_purchased:
-        text = medium_font.render("Purchased", 1, WHITE)
-    else:
-        text = medium_font.render("Upgrade: +1 EPC (200)", 1, WHITE)
-    sc.blit(text, (upgrade_button_rect.x + 10, upgrade_button_rect.y + 25))
-
-    color = (180, 180, 180) if new_planet_purchased else (50, 200, 50)
-    pygame.draw.rect(sc, color, new_planet_button_rect)
-    if new_planet_purchased:
-        text = medium_font.render("Purchased", 1, WHITE)
-    else:
-        text = medium_font.render("New Planet: +5 EPC (500)", 1, WHITE)
-    sc.blit(text, (new_planet_button_rect.x + 10, new_planet_button_rect.y + 25))
+    draw_buttons()
+    draw_flash_texts()
 
     pygame.display.update()
     clock.tick(FPS)
